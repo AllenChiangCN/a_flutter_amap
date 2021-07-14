ddbaecef6eaee42ba8a8fc9eb81d5866781e8ae1
AllenChiangCN <callmeallen55@live.cn>
2021-07-14 15:57:40 +0800

    feat: iOS端添加缩放、拖拽、旋转、倾斜手势

--- ios/Classes/AMapView.swift
+++ ios/Classes/AMapView.swift
@@ -11,20 +11,26 @@ import UIKit
 class AMapView: NSObject, FlutterPlatformView {
     private var _view: UIView
 
-    /**
-     * 初始化后是否自动定位
-     */
+    /// 初始化后是否自动定位
     private var _autoLocateAfterInit: Bool
 
-    /**
-     * 是否显示指南针
-     */
+    /// 是否显示指南针
     private var _showCompass: Bool
 
-    /**
-     * 是否显示比例尺控件
-     */
+    /// 是否显示比例尺控件
     private var _showScaleControl: Bool
+    
+    /// 缩放手势是否可用
+    private var _zoomGestureEnable: Bool
+    
+    /// 旋转手势是否可用
+    private var _rotateGestureEnable: Bool
+    
+    /// 拖拽手势是否可用
+    private var _scrollGestureEnable: Bool
+    
+    /// 倾斜手势是否可用
+    private var _tiltGestureEnable: Bool
 
     init(frame: CGRect, viewIdentifier viewId: Int64, arguments args: Any?, binaryMessenger messenger: FlutterBinaryMessenger?) {
         _view = UIView()
@@ -32,6 +38,10 @@ class AMapView: NSObject, FlutterPlatformView {
         _autoLocateAfterInit = params["autoLocateAfterInit"] as! Bool
         _showCompass = params["showCompass"] as! Bool
         _showScaleControl = params["showScaleControl"] as! Bool
+        _zoomGestureEnable = params["zoomGestureEnable"] as! Bool
+        _rotateGestureEnable = params["rotateGestureEnable"] as! Bool
+        _scrollGestureEnable = params["scrollGestureEnable"] as! Bool
+        _tiltGestureEnable = params["tiltGestureEnable"] as! Bool
         super.init()
         createAMapView(view: _view)
     }
@@ -43,11 +53,13 @@ class AMapView: NSObject, FlutterPlatformView {
     private func createAMapView(view _view: UIView) {
         _view.backgroundColor = UIColor.white
         let mAMapView = MAMapView(frame: _view.bounds)
-        initAMapView(mAMapView)
+        configAMapView(mAMapView)
         _view.addSubview(mAMapView)
     }
 
-    private func initAMapView(_ mapView: MAMapView) {
+    /// 配置地图组件
+    /// - Parameter mapView: 地图组件
+    private func configAMapView(_ mapView: MAMapView) {
         mapView.delegate = self
 
         mapView.showsUserLocation = _autoLocateAfterInit
@@ -58,6 +70,11 @@ class AMapView: NSObject, FlutterPlatformView {
         mapView.showsCompass = _showCompass
 
         mapView.showsScale = _showScaleControl
+        
+        mapView.isZoomEnabled = _zoomGestureEnable
+        mapView.isScrollEnabled = _scrollGestureEnable
+        mapView.isRotateEnabled = _rotateGestureEnable
+        mapView.isRotateCameraEnabled = _tiltGestureEnable
     }
 }
 
